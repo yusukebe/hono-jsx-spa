4fb59c155f162e0f54be1b2cc2ae34e1a56bc1fb
Yusuke Wada <yusuke@kamawada.com>
2025-03-29 07:48:05 +0900

    refactor the `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,29 @@ import react from '@vitejs/plugin-react'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 
 export default defineConfig(({ mode, command }) => {
-  if (command === 'build') {
-    if (mode === 'client') {
-      return {
-        esbuild: {
-          exclude: ['react', 'react-dom']
-        },
-        build: {
-          rollupOptions: {
-            output: {
-              entryFileNames: 'static/client.js'
-            },
-            input: './src/client/index.tsx'
-          }
+  if (command === 'build' && mode === 'client') {
+    return {
+      esbuild: {
+        exclude: ['react', 'react-dom']
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            entryFileNames: 'static/client.js'
+          },
+          input: './src/client/index.tsx'
         }
       }
-    } else {
-      return {}
     }
-  } else {
-    return {
-      plugins: [
-        ssrHotReload({
-          entry: ['./src/server/*'],
-          injectReactRefresh: true
-        }),
-        react(),
-        cloudflare()
-      ]
-    }
+  }
+  return {
+    plugins: [
+      ssrHotReload({
+        entry: ['./src/server/*'],
+        injectReactRefresh: true
+      }),
+      react(),
+      cloudflare()
+    ]
   }
 })
