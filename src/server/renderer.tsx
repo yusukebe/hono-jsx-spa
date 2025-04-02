561b1ca71251728494c7054f9b337559165fdfcf
Yusuke Wada <yusuke@kamawada.com>
2025-04-02 18:55:33 +0900

    fix build steps

--- src/server/renderer.tsx
+++ src/server/renderer.tsx
@@ -1,14 +1,10 @@
-import { env } from 'cloudflare:workers'
 import { reactRenderer } from '@hono/react-renderer'
 
 export const renderer = reactRenderer(({ children }) => {
   return (
     <html>
       <head>
-        <script
-          type="module"
-          src={env.ENVIRONMENT === 'production' ? '/static/client.js' : '/src/client/index.tsx'}
-        ></script>
+        <script type="module" src={import.meta.env.PROD ? '/static/client.js' : '/src/client/index.tsx'}></script>
       </head>
       <body>{children}</body>
     </html>
--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,32 @@ import { defineConfig } from 'vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
+import build from '@hono/vite-build/cloudflare-workers'
 
 export default defineConfig(({ mode, command }) => {
-  if (command === 'build' && mode === 'client') {
-    return {
-      esbuild: {
-        exclude: ['react', 'react-dom']
-      },
-      build: {
-        rollupOptions: {
-          output: {
-            entryFileNames: 'static/client.js'
-          },
-          input: './src/client/index.tsx'
+  if (command === 'build') {
+    if (mode === 'client') {
+      return {
+        esbuild: {
+          exclude: ['react', 'react-dom']
+        },
+        build: {
+          rollupOptions: {
+            output: {
+              entryFileNames: 'static/client.js'
+            },
+            input: './src/client/index.tsx'
+          }
         }
       }
+    }
+    return {
+      resolve: {
+        alias: {
+          'react-dom/server': 'react-dom/server.edge'
+        }
+      },
+      plugins: [build({ entry: './src/server/index.tsx' })]
     }
   }
   return {
