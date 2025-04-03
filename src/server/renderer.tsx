0777cdf98ad12990b4c4d90c893dbb7026e9b671
Yusuke Wada <yusuke@kamawada.com>
2025-04-03 09:17:37 +0900

    fixed

--- src/server/renderer.tsx
+++ src/server/renderer.tsx
@@ -7,7 +7,7 @@ export const renderer = reactRenderer(({ children }) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script type="module" src={import.meta.env.PROD ? '/static/client.js' : '/src/client/index.tsx'}></script>
-        <link href={import.meta.env.PROD ? '/static/assets/style.css' : '/src/src/server/style.css'} rel="stylesheet" />
+        <link href={import.meta.env.PROD ? '/static/assets/style.css' : '/src/server/style.css'} rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
